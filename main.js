3b5c5de7d8b55f9ed4998570e2b9b0dcb781cf8f
dima1501 <dima6343210@yandex.ru>
2021-06-14 14:29:36 +0300

    init

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ function getRandomInt(max) {
 }
 
 function playAudio() {
-    console.log(`assets/sound/sound_${getRandomInt(2) + 1}.mp3`)
     new Audio(`assets/sound/sound_${getRandomInt(2) + 1}.mp3`).play();
 }
